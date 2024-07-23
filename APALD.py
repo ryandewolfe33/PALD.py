18a23e851014c523e4fcb5f8473b7c2788e5b8e4
Ryan DeWolfe <ryandewolfe33@gmail.com>
2024-07-22 21:23:33 -0400

    APALD and many tests

---
+++ APALD.py
@@ -0,0 +1,103 @@
+import scipy.sparse
+import numpy as np
+import pynndescent
+import igraph as ig
+import leidenalg as la
+from tqdm import tqdm
+
+
+"""
+The approximate local pald idea
+
+Some graphs and some intuition suggest
+a) local points have the highest cohesion
+b) we only care about the cohesion of to local points for clustering (i.e. if a point is in a cluster it will be in said cluster with its nearest neighbours)
+
+So, for some point x, y is random from it's knn, and then w is random from (knn_x or knn_y) as a set so double inclusion is not higher probability
+"""
+class APALD:
+    """
+    Create and APALD class and set some algorithm parameters.
+    Parameters:
+        n_neighbors=100: number of neighbors in the nearest neighbor graph.
+        metric="cosine": the metric used for similarity. Anything supported by PyNNDescent is supported.
+    """
+    def __init__(self, n_neighbors=100, metric="cosine"):
+        self.n_neighbors=n_neighbors
+        self.metric=metric
+
+    
+    """
+    Function to compute nearest neighbors and the APALD matrix.
+    Parameters:
+        data, a numpy array with rows for data points columns for data dimensions. 
+    """
+    def fit(self, data):
+        self.index = pynndescent.NNDescent(data, metric=self.metric, n_neighbors=self.n_neighbors)
+
+        knn = self.index.neighbor_graph[0]
+        knn_dist = self.index.neighbor_graph[1]
+
+        cohesion = np.zeros_like(knn_dist)
+
+        for x in tqdm(range(knn.shape[0])):
+            xknn = knn[x]
+            xknn_dist = knn_dist[x]
+
+            other_knn = knn[xknn]
+            other_knn_dist = knn_dist[xknn]
+
+            other_knn_uxy_sizes = np.array([len(np.union1d(xknn, other_knn[i, :])) for i in range(other_knn.shape[0])])
+            for i in range(len(xknn)):
+                dxw = xknn_dist[i]
+                n_further = np.sum(other_knn_dist > dxw, axis=0) + np.sum(other_knn_dist == dxw, axis=0)/2
+                cohesion[x, i] = np.dot(n_further, 1/other_knn_uxy_sizes)
+                
+        row = np.repeat(range(knn.shape[0]), self.n_neighbors)
+        col = knn.flatten()
+        data = cohesion.flatten()
+
+        cohesion = scipy.sparse.csr_matrix((data, (row, col)))
+        self.palds = cohesion.minimum(cohesion.transpose())
+
+        
+    """
+    Function to predict the clusters from the APALD matrix generated in fit.
+    Parameters:
+        thresh=None, Can be a value 0 to 1 or "strong" or None. Cut edges from the APALD graph if below the threshhold. Strong refers to the original PALD paper threshhold of hald the mean diagonal.
+        min_clustert_size=15: Any leiden clusters smaller than 15 are set as noise. Not required for algorithm to function.
+    Returns:
+        np.array of cluster assignments, -1 for noise.
+    """
+    def predict(self, thresh=None, min_cluster_size=15):
+        i,j,weight = scipy.sparse.find(self.palds)
+        if thresh=="strong":
+            thresh = np.mean(self.palds.diagonal())/2
+            
+        if thresh:
+            weight = np.where(weight > thresh, weight, 0)
+
+        self.pald_graph = ig.Graph(n=self.palds.shape[0], edges=list(zip(i, j)))
+        self.pald_graph.es["weight"] = weight
+
+
+        clusters = la.find_partition(self.pald_graph, la.ModularityVertexPartition)
+        self.predict = np.empty(self.palds.shape[0], dtype="int32")
+        current = 0
+        for c in clusters:
+            cn = current
+            if len(c) < min_cluster_size:
+                cn = -1
+            else:
+                current += 1
+            for n in c:
+                self.predict[n] = cn
+        return self.predict
+    
+
+    """
+    Convience Function
+    """
+    def fit_predict(self, data, thresh=None, min_cluster_size=15):
+        self.fit(data)
+        return self.predict(thresh=thresh, min_cluster_size=min_cluster_size)
--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # PALD.py
 
-This is a pure python implementation of the PALD clustering algorithm presented in *A social perspective on perceived distances reveals deep community structure*. The official implemnetation is an R package available at https://github.com/moorekatherine/pald. 
+This is a pure python implementation of the PALD clustering algorithm presented in *A social perspective on perceived distances reveals deep community structure*. The official implemnetation is an R package available at https://github.com/moorekatherine/pald.
+
+# APALD.py
+
+This is a custom work in progress on Approximate PArtitioned Local Depth for clustering. It's showing some promise.  
 
 ## WARNING
 This is by no means an efficient implementation, and the algrothim runs in O(n^3) with the number of points. Usage is not recomended for more than a few hundred points.
 
 Test data was taken from https://github.com/moorekatherine/pald/tree/main/data and converted to csv files (after dropping headers) using https://github.com/vnmabus/rdata. 
+
+Aggregation.txt data is a Benchmark data set Aggregation consisting of n = 788 points.
+Link to data: http://cs.joensuu.fi/sipu/datasets/Aggregation.txt
+A. Gionis, H. Mannila, and P. Tsaparas, Clustering aggregation. ACM Transactions on Knowledge Discovery from Data (TKDD), 2007. 1(1): p. 1-30.
+
+
+Included in "cognate.txt"
+I. Dyen, J. B. Kruskal, P. Black, An Indoeuropean classification: A lexicostatistical experiment.  Trans. Am. Phil. Soc.82, iii-132 (1992)
+Downloaded from: https://github.com/moorekatherine/pald-communities
